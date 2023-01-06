717fadc3553a52336b86e85a933bed06be8ceccd
gguilhermelopes <guilhermelopes275@gmail.com>
2023-01-06 20:59:24 -0300

    debounce removed

--- js/scroll-anima.js
+++ js/scroll-anima.js
@@ -1,11 +1,9 @@
-import debounce from "./debounce.js";
-
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.8;
 
-    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
   // Pega a distância de cada item em relação ao topo do site
